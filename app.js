fbf59ebc9ffd6f832eed6647ccc5670df7357557
weezzzt <wille1tap@outlook.com>
2023-09-18 11:21:41 +0200

    la till kommentar.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// Här använder jag querySelector som använder de id jag gett min button och h1 för att ge det värde till mina variabler. Man kan annars använda getElementById
 const h1text = document.querySelector("#text");
 const button =  document.querySelector("#button");
-
+//Här använder jag mig av en eventlistener som lystnar efter ett click på min button och som sedan ändrar texten till Goodbye World.
 button.addEventListener("click", function() {
     h1text.innerHTML = "Goodbye World"
 });
